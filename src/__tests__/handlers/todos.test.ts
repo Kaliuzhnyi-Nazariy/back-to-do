6e261f30baaffaea81e73f241323c91daf104e69
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-01-07 22:36:36 +0200

    tests

---
+++ src/__tests__/handlers/todos.test.ts
@@ -0,0 +1,16 @@
+import { mockRequest, mockResponse } from "../../__mocks__";
+// import { getToDos } from "../../handlers/todos";
+
+// describe("getToDos", () => {
+//   it("Should return ToDo object in array", () => {
+//     getToDos(mockRequest, mockResponse);
+//     expect(mockResponse.send).toHaveBeenCalledWith([
+//       {
+//         description: "adjfkljgklsadjg",
+//         id: "lala12",
+//         status: "to-do",
+//         title: "lala",
+//       },
+//     ]);
+//   });
+// });
