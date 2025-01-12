d44f1f39f4facc3ba15caa3994e9eac4ecd02c9f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-01-13 00:58:37 +0200

    insert changes into code

--- src/router/todos.ts
+++ src/router/todos.ts
@@ -2,23 +2,27 @@ import { Router } from "express";
 import toDoOperation from "../handlers/todos";
 import middlewares from "../middlewares";
 import todoValidation from "../schemas/validation/todoValidation";
+import "../strategies/local-strategy";
+import { authenticated } from "../helpers/authenticated";
 
 const router = Router();
 
-router.get("/", toDoOperation.getToDos);
+router.get("/", authenticated, toDoOperation.getToDos);
 
 router.post(
   "/",
+  authenticated,
   middlewares.validate(todoValidation),
   toDoOperation.createToDo
 );
 
 router.put(
   "/:id",
+  authenticated,
   middlewares.validate(todoValidation),
   toDoOperation.updateToDo
 );
 
-router.delete("/delete/:id", toDoOperation.deleteToDo);
+router.delete("/delete/:id", authenticated, toDoOperation.deleteToDo);
 
 export default router;
--- src/router/users.ts
+++ src/router/users.ts
@@ -4,6 +4,10 @@ import validate from "../middlewares/validate";
 import userRegisterValidation from "../schemas/validation/user/userRegisterValidation";
 import userLoginValidation from "../schemas/validation/user/userLoginValidation";
 import userUpdateValidation from "../schemas/validation/user/userUpdateValidation";
+import passport, { authenticate } from "passport";
+
+import "../strategies/local-strategy";
+import { authenticated } from "../helpers/authenticated";
 
 const router = Router();
 
@@ -11,14 +15,24 @@ const router = Router();
 
 // router.get("/:id", getUserById);
 
-router.post("/", validate(userRegisterValidation), userCtrl.register);
+router.post(
+  "/",
+  passport.authenticate("local"),
+  validate(userRegisterValidation),
+  userCtrl.register
+);
 
-router.post("/login", validate(userLoginValidation), userCtrl.login);
+router.post(
+  "/login",
+  passport.authenticate("local"),
+  validate(userLoginValidation),
+  userCtrl.login
+);
 
-router.put("/", validate(userUpdateValidation), userCtrl.update);
+router.put("/", authenticated, validate(userUpdateValidation), userCtrl.update);
 
-router.post("/logout", userCtrl.logout);
+router.post("/logout", authenticated, userCtrl.logout);
 
-router.delete("/:id", userCtrl.delete);
+router.delete("/:id", authenticated, userCtrl.delete);
 
 export default router;
