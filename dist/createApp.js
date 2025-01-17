c132ac95670daffc6c884693307cf88863c0f05c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-01-17 19:34:44 +0200

    add all routes and neccessary logic

--- dist/createApp.js
+++ dist/createApp.js
@@ -8,11 +8,35 @@ const express_1 = __importDefault(require("express"));
 const users_1 = __importDefault(require("./router/users"));
 const todos_1 = __importDefault(require("./router/todos"));
 const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const express_session_1 = __importDefault(require("express-session"));
+const notFound_1 = require("./router/notFound");
+const errorRoute_1 = require("./router/errorRoute");
+const passport_1 = __importDefault(require("passport"));
+const connect_mongo_1 = __importDefault(require("connect-mongo"));
 function createApp() {
     const app = (0, express_1.default)();
-    app.use((0, cors_1.default)());
+    const { SECRET_SESSION, DB_HOST } = process.env;
     app.use(express_1.default.json());
+    app.use((0, cookie_parser_1.default)("YbK44CAKbN"));
+    app.use((0, express_session_1.default)({
+        secret: String(SECRET_SESSION),
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 60000 * 60,
+        },
+        store: connect_mongo_1.default.create({
+            // client: mongoose.connection.getClient(),
+            mongoUrl: DB_HOST,
+        }),
+    }));
+    app.use(passport_1.default.initialize());
+    app.use(passport_1.default.session());
+    app.use((0, cors_1.default)());
     app.use("/api/users", users_1.default);
     app.use("/api/todos", todos_1.default);
+    app.use(notFound_1.notFoundRoute);
+    app.use(errorRoute_1.errorRoutes);
     return app;
 }
--- src/createApp.ts
+++ src/createApp.ts
@@ -2,17 +2,56 @@ import express from "express";
 import userRoutes from "./router/users";
 import todosRoutes from "./router/todos";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { notFoundRoute } from "./router/notFound";
+import { errorRoutes } from "./router/errorRoute";
+import { IUser } from "./schemas/types/user.schema";
+import passport from "passport";
+import MongoStore from "connect-mongo";
+
+declare module "express-session" {
+  interface SessionData {
+    visited?: boolean; // Add custom properties here
+    user: IUser;
+    [key: string]: any;
+  }
+}
 
 export function createApp() {
   const app = express();
 
+  const { SECRET_SESSION, DB_HOST } = process.env;
+
   app.use(express.json());
+  app.use(cookieParser("YbK44CAKbN"));
+  app.use(
+    session({
+      secret: String(SECRET_SESSION),
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 60000 * 60,
+      },
+      store: MongoStore.create({
+        // client: mongoose.connection.getClient(),
+        mongoUrl: DB_HOST,
+      }),
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   app.use(cors());
 
   app.use("/api/users", userRoutes);
 
   app.use("/api/todos", todosRoutes);
+
+  app.use(notFoundRoute);
+
+  app.use(errorRoutes);
 
   return app;
 }
